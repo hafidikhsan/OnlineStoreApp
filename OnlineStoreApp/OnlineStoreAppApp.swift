1bd4b7cf92a9d4a6544af57f4d5c50f26dad5b05
hafidikhsan <harifin315@gmail.com>
2024-01-07 17:29:52 +0700

    Initial Commit

---
+++ OnlineStoreApp/OnlineStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreAppApp.swift
+//  OnlineStoreApp
+//
+//  Created by Hafid Ikhsan Arifin on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
